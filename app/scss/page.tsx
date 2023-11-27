020c8ea0e4c9732bad03db3185279533a6c2ee6a
Kidongg <dlrlehd7@daum.net>
2023-11-27 13:19:25 +0900

    scss

--- app/scss/page.tsx
+++ app/scss/page.tsx
@@ -4,7 +4,7 @@ import styles from "./styles.module.scss";
 const page = () => {
   return (
     <div className={styles.nav}>
-      <div className={styles.child1}>SCSS_1</div>
+      <div className={styles.nav_child1}>SCSS_1</div>
       <div className={styles.nav_child2}>SCSS_2</div>
     </div>
   );
