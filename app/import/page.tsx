bbc683dbee02a454e38538708b0d7002fc0e0ca6
Kidongg <dlrlehd7@daum.net>
2023-11-28 10:54:54 +0900

    import

---
+++ app/import/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "./styles.module.scss";
+
+const page = () => {
+  return (
+    <div>
+      <div className={styles.nav_1}>IMPORT_1</div>
+      <div className={styles.nav_2}>IMPORT_2</div>
+      <div className={styles.nav_3}>IMPORT_3</div>
+    </div>
+  );
+};
+
+export default page;
