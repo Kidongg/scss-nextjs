8cd8df3db5d09208272e619f860b02b959f59ff1
Kidongg <dlrlehd7@daum.net>
2023-11-28 10:13:16 +0900

    mixin

---
+++ app/mixin/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "./styles.module.scss";
+
+const page = () => {
+  return (
+    <div>
+      <div className={styles.nav_1}>MIXIN_1</div>
+      <div className={styles.nav_2}>MIXIN_1</div>
+    </div>
+  );
+};
+
+export default page;
