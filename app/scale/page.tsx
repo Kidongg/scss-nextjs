d4aedcc616a0723b2271ab887706b1f94f35a284
Kidongg <dlrlehd7@daum.net>
2023-11-30 13:59:05 +0900

    scale

---
+++ app/scale/page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./styles.module.scss";
+
+const page = () => {
+  return <button className={styles.btn}>Hover me</button>;
+};
+
+export default page;
