5eb2e3b66c3823013fc942d26bcfa39decec2d7f
Kidongg <dlrlehd7@daum.net>
2023-11-30 11:26:34 +0900

    for

--- app/for/page.tsx
+++ app/for/page.tsx
@@ -1,7 +1,20 @@
 import React from "react";
+import styles from "./styles.module.scss";
 
 const page = () => {
-  return <div>page</div>;
+  return (
+    <div>
+      <div className={styles.item}></div>
+      <div className={styles.item}></div>
+      <div className={styles.item}></div>
+      <div className={styles.item}></div>
+      <div className={styles.item}></div>
+      <div className={styles.item}></div>
+      <div className={styles.item}></div>
+      <div className={styles.item}></div>
+      <div className={styles.item}></div>
+    </div>
+  );
 };
 
 export default page;
