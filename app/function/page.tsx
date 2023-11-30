4a2ce48f45bac506048f25a973ca98755a8757d4
Kidongg <dlrlehd7@daum.net>
2023-11-30 15:10:32 +0900

    function

---
+++ app/function/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "./styles.module.scss";
+import "./styles.scss";
+
+const page = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.icon}></div>
+      <div className={styles.icon2}></div>
+    </div>
+  );
+};
+
+export default page;
