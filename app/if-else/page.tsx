38597bed2d5db02aeb8cd45b501912dddd72cbf9
Kidongg <dlrlehd7@daum.net>
2023-11-30 11:17:56 +0900

    if-else

---
+++ app/if-else/page.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import styles from "./styles.module.scss";
+
+const page = () => {
+  return (
+    <div>
+      <div className={styles.text}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tortor
+        dui, bibendum gravida mi in, porta faucibus mi. Ut vitae ipsum dictum,
+        porta quam non, sollicitudin ipsum. Sed sit amet placerat dui, id
+        commodo mauris. Nulla a scelerisque libero. Sed semper mauris nec ex
+        hendrerit, ut volutpat arcu interdum. Aenean ultricies vestibulum nulla,
+        sed fringilla sapien posuere sit amet. Etiam ullamcorper pharetra
+        pharetra. Suspendisse accumsan massa non sem auctor egestas. Nulla non
+        nisi dolor. Proin ut condimentum dui. Integer mattis interdum nunc vitae
+        varius. Duis urna tellus, consequat eget magna in, condimentum pharetra
+        mauris. Aliquam fringilla mattis nulla, in luctus mi.
+      </div>
+      <br />
+      <div className={styles.long_text}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tortor
+        dui, bibendum gravida mi in, porta faucibus mi. Ut vitae ipsum dictum,
+        porta quam non, sollicitudin ipsum. Sed sit amet placerat dui, id
+        commodo mauris. Nulla a scelerisque libero. Sed semper mauris nec ex
+        hendrerit, ut volutpat arcu interdum. Aenean ultricies vestibulum nulla,
+        sed fringilla sapien posuere sit amet. Etiam ullamcorper pharetra
+        pharetra. Suspendisse accumsan massa non sem auctor egestas. Nulla non
+        nisi dolor. Proin ut condimentum dui. Integer mattis interdum nunc vitae
+        varius. Duis urna tellus, consequat eget magna in, condimentum pharetra
+        mauris. Aliquam fringilla mattis nulla, in luctus mi.
+      </div>
+    </div>
+  );
+};
+
+export default page;
