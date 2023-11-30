eed8b446c54818829da84d2442a2444d9c6c4bc3
Kidongg <dlrlehd7@daum.net>
2023-11-30 11:39:53 +0900

    use

---
+++ app/use/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./styles.module.scss";
+
+const page = () => {
+  return (
+    <div className={styles.btn_container}>
+      <div className={styles.btn_container_box}></div>
+    </div>
+  );
+};
+
+export default page;
