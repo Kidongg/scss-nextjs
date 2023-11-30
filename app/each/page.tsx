4da1b1673c9e651d45167a88af8b019a3a1b8242
Kidongg <dlrlehd7@daum.net>
2023-11-30 13:50:13 +0900

    while

--- app/each/page.tsx
+++ app/each/page.tsx
@@ -3,7 +3,7 @@ import styles from "./styles.module.scss";
 
 const page = () => {
   return (
-    <div className={styles.footer}>
+    <div className={styles.container}>
       <div className={`${styles.sns} ${styles.facebook}`}></div>
       <div className={`${styles.sns} ${styles.youtube}`}></div>
       <div className={`${styles.sns} ${styles.instagram}`}></div>
---
+++ app/while/page.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import styles from "./styles.module.scss";
+
+const page = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.item}>😴</div>
+      <div className={styles.item}>😴</div>
+      <div className={styles.item}>😴</div>
+      <div className={styles.item}>😴</div>
+      <div className={styles.item}>😚</div>
+    </div>
+  );
+};
+
+export default page;
